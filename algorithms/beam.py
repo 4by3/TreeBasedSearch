e640f692eda562bcb4c85608ea7c4d5e28fbb69e
4by3 <timothywastaken@hotmail.com>
2025-04-07 17:32:33 +1000

    Added CUS2 algorithm beam search

---
+++ algorithms/beam.py
@@ -0,0 +1,29 @@
+import heapq
+from algorithms.heuristic_euclidean import heuristic_euclidean
+
+def beam(graph, nodes, start, goals, beam_width=2):
+    priority_queue = []
+    heapq.heappush(priority_queue, (heuristic_euclidean(start, nodes, goals), start, [start]))
+
+    visited = set()
+    number_of_nodes = 0
+
+    while priority_queue:
+        _, current_node, path = heapq.heappop(priority_queue)
+
+        if current_node in visited:
+            continue
+        visited.add(current_node)
+        number_of_nodes += 1
+
+        if current_node in goals:
+            return current_node, number_of_nodes, path
+
+        for neighbor, _ in graph.get(current_node, []):
+            if neighbor not in visited:
+                heapq.heappush(priority_queue, (heuristic_euclidean(neighbor, nodes, goals), neighbor, path + [neighbor]))
+
+        # Prune priority queue based on beam
+        priority_queue = heapq.nsmallest(beam_width, priority_queue)
+
+    return None, number_of_nodes, []
