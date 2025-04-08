64812cc124eac51e5549718732fa806a289855a3
4by3 <timothywastaken@hotmail.com>
2025-04-09 00:30:10 +1000

    Added cost

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -1,10 +1,10 @@
 def dfs(graph, start, goals):
-    stack = [(start, [start])]
+    stack = [(0, start, [start])]
     visited = set()
     number_of_nodes = 0
 
     while stack:
-        node, path = stack.pop()
+        cost, node, path = stack.pop()
 
         if node in visited:
             continue
@@ -12,11 +12,11 @@ def dfs(graph, start, goals):
         number_of_nodes += 1
 
         if node in goals:
-            return node, number_of_nodes, path  # Goal found
+            return node, number_of_nodes, path, cost
 
-        if node in graph:  # Fix: Access graph directly, not graph.edges
-            for neighbor, _ in sorted(graph[node], reverse=True):
+        if node in graph:
+            for neighbor, node_cost in sorted(graph[node], reverse=True):
                 if neighbor not in visited:
-                    stack.append((neighbor, path + [neighbor]))
+                    stack.append((cost + node_cost, neighbor, path + [neighbor]))
 
     return None, number_of_nodes, []  # No valid path found
