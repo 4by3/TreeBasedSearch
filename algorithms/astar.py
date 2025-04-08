fffdf7ee626085bd0a2daa6e3b459cd8107792f5
4by3 <timothywastaken@hotmail.com>
2025-04-09 00:29:11 +1000

    Fixed how total cost is calculated

--- algorithms/astar.py
+++ algorithms/astar.py
@@ -9,14 +9,13 @@ def astar(graph, nodes, start, goals):
     #Need to account for multiple goals: select the goal closest to the origin
     
     priority_queue = []    
-    heapq.heappush(priority_queue, (heuristic_euclidean(start, nodes, goals), start, [start]))
+    heapq.heappush(priority_queue, (heuristic_euclidean(start, nodes, goals), 0, start, [start]))
     
-    total_cost = 0
     number_of_nodes = 0
     visited = set()
     
     while priority_queue:
-        node_cost, current_node, path = heapq.heappop(priority_queue)
+        fNode, cost, current_node, path = heapq.heappop(priority_queue)
         
         if current_node in visited:
             continue
@@ -24,20 +23,11 @@ def astar(graph, nodes, start, goals):
         number_of_nodes += 1
         
         if current_node in goals:
-            return current_node, number_of_nodes, path, node_cost
+            return current_node, number_of_nodes, path, cost
         
         for neighbor, edge_cost in graph.get(current_node, []):
             if neighbor not in visited:
-                heapq.heappush(priority_queue, (heuristic_euclidean(neighbor, nodes, goals) + edge_cost + total_cost, neighbor, path + [neighbor]))
+                edge_fNode = heuristic_euclidean(neighbor, nodes, goals) + cost + edge_cost
+                heapq.heappush(priority_queue, (edge_fNode, cost + edge_cost, neighbor, path + [neighbor]))
     
     return None, number_of_nodes, []
-                
-
-        
-    
-    
-
-    
-
-
-
