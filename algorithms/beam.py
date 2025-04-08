58970aa117bd57bc771d2e011c384975ac95ae1c
4by3 <timothywastaken@hotmail.com>
2025-04-09 00:49:05 +1000

    Fixed beam cost

--- algorithms/beam.py
+++ algorithms/beam.py
@@ -1,15 +1,18 @@
 import heapq
 from algorithms.heuristic_euclidean import heuristic_euclidean
 
-def beam(graph, nodes, start, goals, beam_width=2):
+def beam(graph, nodes, start, goals):
     priority_queue = []
-    heapq.heappush(priority_queue, (heuristic_euclidean(start, nodes, goals), start, [start]))
+    heapq.heappush(priority_queue, (heuristic_euclidean(start, nodes, goals), 0, start, [start]))
 
     visited = set()
     number_of_nodes = 0
 
+    # Beam width
+    beam_width = 2
+
     while priority_queue:
-        cost, current_node, path = heapq.heappop(priority_queue)
+        fNode, cost, current_node, path = heapq.heappop(priority_queue)
 
         if current_node in visited:
             continue
@@ -19,9 +22,9 @@ def beam(graph, nodes, start, goals, beam_width=2):
         if current_node in goals:
             return current_node, number_of_nodes, path, cost
 
-        for neighbor, _ in graph.get(current_node, []):
+        for neighbor, node_cost in graph.get(current_node, []):
             if neighbor not in visited:
-                heapq.heappush(priority_queue, (heuristic_euclidean(neighbor, nodes, goals), neighbor, path + [neighbor]))
+                heapq.heappush(priority_queue, (heuristic_euclidean(neighbor, nodes, goals), cost + node_cost, neighbor, path + [neighbor]))
 
         # Prune priority queue based on beam
         priority_queue = heapq.nsmallest(beam_width, priority_queue)
