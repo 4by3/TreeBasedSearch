87749ea16e65e70077fad5835de87763a46be465
4by3 <timothywastaken@hotmail.com>
2025-04-13 02:01:57 +1000

    Updated dijkstra

--- algorithms/dijkstra.py
+++ algorithms/dijkstra.py
@@ -1,29 +1,34 @@
 import heapq
 
+# Time complexity is O(edges * log(nodes)) due to use of heap, which is longer compared to DFS and BFS
 def dijkstra(graph, start, goals):
+    # Set first value in priority queue to starting node
     priority_queue = [(0, start, [start])]
     visited = set()
     number_of_nodes = 0
-    shortest_paths = {start: 0}
 
     while priority_queue:
         cost, node, path = heapq.heappop(priority_queue)
 
+        # Skip visited node
         if node in visited:
             continue
         visited.add(node)
         number_of_nodes += 1
 
+        # Return path found
         if node in goals:
             return node, number_of_nodes, path, cost
 
         if node in graph:
             for neighbor, edge_cost in graph[node]:
+                # Calculating total cost of this neighbour node by adding current node cost (total) with neighbour cost
                 new_cost = cost + edge_cost
-                if neighbor not in shortest_paths or new_cost < shortest_paths[neighbor]:
-                    shortest_paths[neighbor] = new_cost
+                if neighbor not in visited:
+                    # Adding total cost in first so that heap sorts based on cost value
                     heapq.heappush(priority_queue, (new_cost, neighbor, path + [neighbor]))
 
+    # Return no path found
     return None, number_of_nodes, [], None
 
 
